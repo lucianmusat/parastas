71226d6ffa62ea886e456727868176de3df2c3ea
Lucian Musat <amibadesign@gmail.com>
2023-06-20 20:00:33 +0200

    Initial commit

---
+++ src/main/java/online/lucianmusat/Parastas/ParastasApplication.java
@@ -0,0 +1,13 @@
+package online.lucianmusat.Parastas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParastasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParastasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/online/lucianmusat/Parastas/ParastasApplicationTests.java
@@ -0,0 +1,13 @@
+package online.lucianmusat.Parastas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParastasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
