ebb3ca2da0bbbad40861959420e4639d1b7420ce
Lucian Musat <lucian@mediadistillery.com>
2023-06-21 19:35:41 +0200

    Some progress

--- src/main/java/online/lucianmusat/Parastas/ParastasApplication.java
+++ src/main/java/online/lucianmusat/Parastas/ParastasApplication.java
@@ -1,14 +1,20 @@
 package online.lucianmusat.Parastas;
 
 import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.model.Container;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.core.DockerClientImpl;
+import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
+import com.github.dockerjava.transport.DockerHttpClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 
-import java.util.Properties;
+import java.time.Duration;
+import java.util.List;
+
 
 @SpringBootApplication
 public class ParastasApplication {
@@ -20,17 +26,15 @@ public class ParastasApplication {
 	@EventListener(ApplicationReadyEvent.class)
 	public void doSomethingAfterStartup() {
 		System.out.println("Hello World!");
-		Properties properties = new Properties();
-		properties.setProperty("registry.email", "info@baeldung.com");
-		properties.setProperty("registry.password", "baeldung");
-		properties.setProperty("registry.username", "baaldung");
-		properties.setProperty("DOCKER_HOST", "tcp://docker.baeldung.com:2376");
-
 		DefaultDockerClientConfig config
-				= DefaultDockerClientConfig.createDefaultConfigBuilder()
-				.withProperties(properties).build();
-
-		DockerClient dockerClient = DockerClientBuilder.getInstance(config).build();
+				= DefaultDockerClientConfig.createDefaultConfigBuilder().build();
+		DockerHttpClient httpClient = new ApacheDockerHttpClient.Builder()
+				.dockerHost(config.getDockerHost())
+				.sslConfig(config.getSSLConfig())
+				.build();
+		DockerClient dockerClient = DockerClientImpl.getInstance(config, httpClient);
+		List<Container> containers = dockerClient.listContainersCmd().exec();
+		System.out.println(containers.size());
 	}
 
 }
